7c06191b9a855011b2a199f92a8ae344f0c00174
GyroZepelix <gamingskullgamer@gmail.com>
2024-03-12 20:22:50 +0100

    Removed debugging log

--- datagen/src/__main__.py
+++ datagen/src/__main__.py
@@ -2,7 +2,6 @@ from lib import *
 
 def main():
     inputs = read_inputs()
-    print(inputs)
     template = load_template(inputs)
     template_vars = load_variables(inputs)
     process_templates(inputs, template, template_vars)
