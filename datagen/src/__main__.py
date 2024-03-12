5779e7ad704a4ce1410adcf05da5f5ab0c0ea406
GyroZepelix <gamingskullgamer@gmail.com>
2024-03-12 20:09:33 +0100

    Made a cli tool for generating the needed jsons

--- datagen/src/__main__.py
+++ datagen/src/__main__.py
@@ -1,1 +1,11 @@
-print("hello")
+from lib import *
+
+def main():
+    inputs = read_inputs()
+    print(inputs)
+    template = load_template(inputs)
+    template_vars = load_variables(inputs)
+    process_templates(inputs, template, template_vars)
+
+if __name__ == "__main__":
+    main()
---
+++ datagen/src/lib.py
@@ -0,0 +1,41 @@
+import json
+import argparse
+import os
+
+def read_inputs():
+    """Read inputs from the command line."""
+    parser = argparse.ArgumentParser(description='Process some templates.')
+    parser = argparse.ArgumentParser(description='Process some templates.')
+    parser.add_argument('--template', help='Path to the template file', required=True)
+    parser.add_argument('--variables', help='Path to the variables JSON file', required=True)
+    parser.add_argument('--output', help='Output directory', default='../target')
+    parser.add_argument('--output_name', help='Name of the output JSON file', default='output.json')
+    args = parser.parse_args()
+    return args
+
+def load_template(args):
+    """Load the template from a file."""
+    with open(args.template, 'r') as file:
+        template = file.read()
+    return template
+
+def load_variables(args):
+    """Load variables from a JSON file."""
+    with open(args.variables, 'r') as file:
+        variables = json.load(file)
+    return variables
+
+def process_templates(args, template, template_vars):
+    """Process the template with the given variables and save to subfolders."""
+    for key, value in template_vars.items():
+        processed_template = template
+        for var_key, var_value in value.items():
+            processed_template = processed_template.replace(f"%{var_key}%", str(var_value))
+        
+        # Create subfolder for each key
+        output_path = os.path.join(args.output, key)
+        os.makedirs(output_path, exist_ok=True)
+        
+        # Save processed template in the subfolder
+        with open(os.path.join(output_path, args.output_name), 'w') as file:
+            file.write(processed_template)
